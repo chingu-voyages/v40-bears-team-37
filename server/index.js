99193a7f05326fb95ece68d37820e40137492701
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-08-13 17:26:28 +0800

    feat: set up express server and dotenv

--- server/index.js
+++ server/index.js
@@ -1,3 +1,17 @@
-const hello = require("client")
+const dotenv = require("dotenv")
+const express = require('express');
 
-hello()
+// grab sensitive data from .env file
+dotenv.config()
+
+// instantiate express app
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.get('/', (req, res) => {
+  res.send('Express is set up!');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
