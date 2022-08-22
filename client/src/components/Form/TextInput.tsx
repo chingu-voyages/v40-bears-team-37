441ab037bc5df82ae4fade1e116cc685cd60ed79
Ines Lin <ineslin35@gmail.com>
2022-08-23 01:29:36 +0800

    style: remove useless React hooks

--- client/src/components/Form/TextInput.tsx
+++ client/src/components/Form/TextInput.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef } from 'react';
 import styled from "styled-components";
 
 interface FormProps {
