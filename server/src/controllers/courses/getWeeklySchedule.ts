7ff34c2853d89e000b67ee7e7e43f639b77bec71
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-08-23 22:49:47 +0800

    trim response

--- server/src/controllers/courses/getWeeklySchedule.ts
+++ server/src/controllers/courses/getWeeklySchedule.ts
@@ -7,7 +7,6 @@ import {
   WeekDays,
 } from "../../helpers/weeklySchedule";
 import { getUserId } from "../../helpers/user";
-import moment from "moment";
 import User from "../../models/user.model";
 import Course, { CourseDocument } from "../../models/course.model";
 import { WeeklyScheduleQueryType } from "../../validators/courses";
@@ -38,15 +37,17 @@ export const getWeeklySchedule = async (req: Request, res: Response) => {
 
     const results = {
       week_id: Number(week[1]),
-      month: moment(week[1]).format("MMMM"),
-      year: moment(week[1]).format("YYYY"),
-      start_date: moment(week[0]).format("Do"),
-      end_date: moment(week[6]).format("Do"),
+      // month: moment(week[1]).format("MMMM"),
+      // year: moment(week[1]).format("YYYY"),
+      // start_date: moment(week[0]).format("Do"),
+      // end_date: moment(week[6]).format("Do"),
+      start_date: Number(week[0]),
+      end_date: Number(week[6]),
       prev_week_id: Number(prevWeekId),
       next_week_id: Number(nextWeekId),
       schedules: Object.keys(structuredWeekLessons).map((day, index) => ({
         day,
-        date_label: moment(week[index + 1]).format("MMMM Do"),
+        // date_label: moment(week[index + 1]).format("MMMM Do"),
         date: Number(week[index + 1]),
         lessons: structuredWeekLessons[day as WeekDays],
       })),
