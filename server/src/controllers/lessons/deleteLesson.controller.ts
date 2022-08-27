fff9cbf9538d952ab031b7dcf2b87ae404f0e8bf
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-08-27 19:31:39 +0800

    refactor

--- server/src/controllers/lessons/deleteLesson.controller.ts
+++ server/src/controllers/lessons/deleteLesson.controller.ts
@@ -1,19 +1,12 @@
 import { Request, Response } from "express";
-import { getLessonById } from "../../services/getLessons";
-import { getUserId } from "../../helpers/user";
 import { Types } from "mongoose";
 import { deleteLesson } from "../../services/deleteLesson";
 
 export default async function (req: Request, res: Response) {
-  const userId = getUserId(req);
   const { lessonId } = req.params;
 
   try {
-    const { lesson } = await getLessonById(
-      new Types.ObjectId(lessonId),
-      userId
-    );
-    await deleteLesson(lesson._id);
+    await deleteLesson(new Types.ObjectId(lessonId));
   } catch (e) {
     return res.status(400).json({ success: false, message: e.message });
   }
