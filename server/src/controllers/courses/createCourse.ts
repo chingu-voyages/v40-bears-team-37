867c8a420ed3df164c93bf0de04cb6db05a2d1e5
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-08-27 17:58:29 +0800

    refactored /courses POST route Separated database logic into a service function. Changed status code from 200 to 201 for success. Created consistent return format for data.

--- server/src/controllers/courses/createCourse.ts
+++ server/src/controllers/courses/createCourse.ts
@@ -1,32 +1,19 @@
 import { Request, Response } from "express";
+import { getUserId } from "../../helpers/user";
+import createCourseInDB from "../../services/createCourse";
 
 export default async function createCourse(req: Request, res: Response) {
     try {
-        const newCourse = await createCourseInDB(req.body)
-        return res.status(200).json(newCourse)
+        const userId = getUserId(req)
+        const newCourse = await createCourseInDB(req.body, userId)
+        return res.status(201).json({
+            success: true,
+            data: newCourse
+        })
     } catch(e) {
         return res.status(400).json({
             success: false,
             message: e.message
         })
-    }
-}
-
-import Course from "../../models/course.model"
-import { CoursePayloadType } from "../../validators/courses"
-
-async function createCourseInDB(body: CoursePayloadType) {
-    try {
-        const newCourse = new Course({
-            name: body.name,
-            start_date: body.start_date,
-            end_date: body.end_date,
-            color: body.color,
-            weekly_schedule: body.weekly_schedule
-        })
-        await newCourse.save()
-        return newCourse
-    } catch(e) {
-        throw new Error(e)
     }
 }
