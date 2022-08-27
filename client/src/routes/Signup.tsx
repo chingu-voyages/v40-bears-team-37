7cbf58d384581cf8db40be8fc3808b86f9b9c0a8
Cheryl Murphy <webmaster@cherylli.com>
2022-08-27 23:31:55 +1000

    revert changes on signup, fix frontend url on server

--- client/src/routes/Signup.tsx
+++ client/src/routes/Signup.tsx
@@ -2,36 +2,19 @@ import {z} from "zod";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Link} from "react-router-dom";
-
-import {
-    SignUpStyles,
-    CarouselStyles,
-    FormStyle,
-    InputFormStyles,
-    AuthNavigationStyles,
-} from "../styles/AuthFormStyles";
+import {SignUpStyles, CarouselStyles, FormStyle, InputFormStyles, AuthNavigationStyles} from "../styles/AuthFormStyles";
+import {signup as signupService} from 'services/auth';
+import {SignupResponseType, UserType} from 'types/auth';
+import {useAuth} from 'context/AuthContext';
+import {AxiosResponse} from 'axios';
 
 const signUpValidation = z
     .object({
-        username: z
-            .string({required_error: "Must provide username."})
-            .trim()
-            .min(3, "Username should be longer than 3 characters.")
-            .max(30, "Your username is too long."),
-        email: z
-            .string()
-            .trim()
-            .email("Must provide email.")
-            .max(50, "Your Email is too long."),
-        password: z
-            .string({required_error: "Must provide password."})
-            .trim()
-            .min(6, "Password should be longer than 10 characters.")
-            .max(50, "Your password is too long."),
-        confirmPassword: z
-            .string({required_error: "Must provide confirm password"})
-            .trim()
-            .max(50, "Your confirm password is too long."),
+        username: z.string({required_error: "Must provide username"}).trim().min(3, "Username should be longer than 3 characters.").max(30, "Your username is too long."),
+        email: z.string().trim().email("Must provide email.").max(50, "Your Email is too long."),
+        password: z.string({required_error: "Must provide password"}).trim().min(6, "Password should be longer than 10 characters").max(50, "Your password is too long."),
+        confirmPassword: z.string({required_error: "Must provide confirm password"}).trim().max(50, "Your confirm password is too long."),
+        signupError: z.void()
     })
     .refine((data) => data.password === data.confirmPassword, {
         message: "Passwords don't match.",
@@ -41,28 +24,37 @@ const signUpValidation = z
 type SignUpFieldsType = z.infer<typeof signUpValidation>;
 
 function SignUp() {
+    const auth = useAuth()
     const {
         register,
         handleSubmit,
-        reset,
         setError,
-        setValue,
-        getValues,
-        formState: {errors},
-    } = useForm({
+        formState: {errors, isSubmitSuccessful},
+    } = useForm<z.output<typeof signUpValidation>>({
         defaultValues: {
             username: "",
             email: "",
             password: "",
-            confirmPassword: "",
+            confirmPassword: ""
         },
         resolver: zodResolver(signUpValidation),
     });
 
-    const onSubmit = (values: SignUpFieldsType) => {
-        // TODO: API call to sign up an account
-        reset();
-    };
+    const onSubmit = async (values: SignUpFieldsType) => {
+        const data = await signupService({
+            "name": values.username,
+            "password": values.password,
+            "email": values.email
+        }) as SignupResponseType
+        if (data.success === false) {
+            setError('signupError', {message: data.message})
+        } else {
+            // Successful Signup
+            console.log(data.data)
+            // TODO: should not need to log in again. register starts a new session
+            //auth.login(data.data as UserType)
+        }
+    }
 
     const inputs = [
         {
@@ -70,28 +62,28 @@ function SignUp() {
             label: "Username",
             registerKey: "username" as const,
             errorCondition: errors.username,
-            inputType: "text",
+            inputType: "text"
         },
         {
             id: "sign-up-email",
             label: "Email Address",
             registerKey: "email" as const,
             errorCondition: errors.email,
-            inputType: "text",
+            inputType: "text"
         },
         {
             id: "sign-up-password",
             label: "Password",
             registerKey: "password" as const,
             errorCondition: errors.password,
-            inputType: "password",
+            inputType: "password"
         },
         {
             id: "sign-up-confirm-password",
             label: "Confirm Password",
             registerKey: "confirmPassword" as const,
             errorCondition: errors.confirmPassword,
-            inputType: "password",
+            inputType: "password"
         },
     ];
 
@@ -100,37 +92,35 @@ function SignUp() {
             <CarouselStyles>Carousel View</CarouselStyles>
             <FormStyle>
                 <h1>Sign Up</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    {inputs.map(
-                        ({id, label, registerKey, errorCondition, inputType}) => {
-                            return (
-                                <InputFormStyles key={id}>
-                                    <label htmlFor={id}>{label}</label>
-                                    <input
-                                        id={id}
-                                        type={inputType}
-                                        {...register(registerKey)}
-                                        className={errorCondition && "invalid"}
-                                    ></input>
-                                    {errorCondition && (
-                                        <small className="error-message">
-                                            {errorCondition.message}
-                                        </small>
-                                    )}
-                                </InputFormStyles>
-                            );
-                        }
-                    )}
-                    <button type="submit">Get My Notum</button>
-                </form>
+                {!isSubmitSuccessful ? (
+                    <>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            {inputs.map(({id, label, registerKey, errorCondition, inputType}) => {
+                                return (
+                                    <InputFormStyles key={id}>
+                                        <label htmlFor={id}>{label}</label>
+                                        <input id={id} type={inputType} {...register(registerKey)}
+                                               className={errorCondition && 'invalid'}></input>
+                                        {errorCondition &&
+                                            <small className="error-message">{errorCondition.message}</small>}
+                                    </InputFormStyles>
+                                )
+                            })}
+                            {errors.signupError && <p>{errors.signupError.message}</p>}
+                            <button type="submit">Get My Notum</button>
+                        </form>
+                        <AuthNavigationStyles>
+                            <p>Already have an account? <Link to="/login">Login Here</Link></p>
+                        </AuthNavigationStyles>
+                    </>
+                ) : (<p>Thank you for signing up</p>)}
                 <AuthNavigationStyles>
-                    <p>
-                        Already have an account? <Link to="/login">Login Here</Link>
-                    </p>
+                    {/*TODO: might need to change this*/}
+                    <p><Link to="/">Go to calendar</Link></p>
                 </AuthNavigationStyles>
             </FormStyle>
         </SignUpStyles>
     );
 }
 
-export default SignUp;
+export default SignUp
--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ const app = express();
 // middlewares
 app.use(
     cors({
-        origin: IS_PROD ? FRONTEND_URL : "http://localhost:4000",
+        origin: IS_PROD ? FRONTEND_URL : "http://localhost:3000",
         credentials: true,
     })
 );
