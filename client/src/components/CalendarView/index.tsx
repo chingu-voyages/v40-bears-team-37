8339feb5bf65fbb6704c8db80fa54f1433017290
Ines Lin <ineslin35@gmail.com>
2022-08-28 23:24:51 +0800

    feat: the draft of fetching weekly-schedule

--- client/src/components/CalendarView/index.tsx
+++ client/src/components/CalendarView/index.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import CalendarWeek from "components/CalendarView/CalendarWeek";
 import data from "api/dummy-data.json";
 
+import { getWeeklySchedule as getWeeklyScheduleService } from "../../services/courses";
+import { useEffect } from "react";
+
 const CalendarViewStyles = styled.div`
   display: flex;
   margin: 0 auto;
@@ -9,6 +12,16 @@ const CalendarViewStyles = styled.div`
 
 const CalendarView = () => {
   const { week } = data;
+
+  useEffect(() => {
+    const getWeeklySchedule = async () => {
+      const weeklyScheduleData = await getWeeklyScheduleService({
+        weekId: undefined
+      })
+    };
+    getWeeklySchedule();
+  }, []);
+  
   return (
     <CalendarViewStyles>
       <CalendarWeek week={week} />
