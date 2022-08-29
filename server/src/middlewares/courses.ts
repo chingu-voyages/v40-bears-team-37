f491297eed225a0cf02a6358c306700cf293cc74
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-08-29 20:55:58 +0800

    reduced number of db calls

--- server/src/middlewares/courses.ts
+++ server/src/middlewares/courses.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import { Types } from "mongoose";
 import getCourse from "../services/getCourse";
 import { getUserId } from "../helpers/user";
-import getUser from "../services/getUser";
 import User from "../models/user.model";
 
 export default async function isAuthorizedToModifyCourse(req: Request, res: Response, next: NextFunction) {
@@ -18,30 +17,17 @@ export default async function isAuthorizedToModifyCourse(req: Request, res: Resp
         }
 
         const userId = getUserId(req)
-        const user = await getUser(userId)
-        if (user === null) {
+        const userWhoOwnsCourse = await User.findOne({
+            courses: new Types.ObjectId(courseId),
+            _id: userId
+        })
+        if (userWhoOwnsCourse === null) {
             return res.status(401).json({
                 success: false,
-                message: "You do not have the authorization to modify this course. Please log in."
+                message: "Either user does not exist or user does not have permission to modify this course." 
             })
         }
-
-        const userWhichOwnsCourse = await User.findOne({ courses: courseId})
-        if (userWhichOwnsCourse === null) {
-            return res.status(400).json({
-                success: false,
-                message: "This course does not belong to a user." 
-            })
-        }
-        if (!userWhichOwnsCourse._id.equals(userId)) {
-            return res.status(401).json({
-                success: false,
-                message: "You are not the owner of this course. Access denied."
-            })
-        }
-
         next()
-
     } catch(e) {
         return res.status(500).json({
             success: false,
