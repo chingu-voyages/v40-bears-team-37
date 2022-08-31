3629f64f6fa83e5171d5a22aa32f7c04e53fc7c8
Cheryl Murphy <webmaster@cherylli.com>
2022-08-31 22:06:14 +1000

    add 404 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import PrivateRoute from "components/Auth/PrivateRoute";
 import Search from "routes/Search";
 import Settings from "routes/Settings";
@@ -7,58 +7,65 @@ import Login from "routes/Login";
 import Signup from "routes/Signup";
 import React from "react";
 import CalendarPage from "routes/Calender";
-import { useAuth } from "context/AuthContext";
+import {useAuth} from "context/AuthContext";
 import Loader from "components/Loader/Loader";
+import PageNotFound from 'routes/404';
 
 function App() {
-  const { isCheckingAuth } = useAuth();
+    const {isCheckingAuth} = useAuth();
 
-  if (isCheckingAuth) return <Loader />;
+    if (isCheckingAuth) return <Loader/>;
 
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <CalendarPage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/search"
-        element={
-          <PrivateRoute>
-            <Search />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/settings"
-        element={
-          <PrivateRoute>
-            <Settings />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <PublicRoute>
-            <Login />
-          </PublicRoute>
-        }
-      />
-      <Route
-        path="/signup"
-        element={
-          <PublicRoute>
-            <Signup />
-          </PublicRoute>
-        }
-      />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <PrivateRoute>
+                        <CalendarPage/>
+                    </PrivateRoute>
+                }
+            />
+            <Route
+                path="/search"
+                element={
+                    <PrivateRoute>
+                        <Search/>
+                    </PrivateRoute>
+                }
+            />
+            <Route
+                path="/settings"
+                element={
+                    <PrivateRoute>
+                        <Settings/>
+                    </PrivateRoute>
+                }
+            />
+            <Route
+                path="/login"
+                element={
+                    <PublicRoute>
+                        <Login/>
+                    </PublicRoute>
+                }
+            />
+            <Route
+                path="/signup"
+                element={
+                    <PublicRoute>
+                        <Signup/>
+                    </PublicRoute>
+                }
+            />
+            <Route
+                path="*"
+                element={
+                    <PageNotFound/>
+                }
+            />
+        </Routes>
+    );
 }
 
 export default App;
---
+++ client/src/routes/404.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const PageNotFoundStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  a:hover {
+    text-decoration: underline;
+  }
+`
+const PageNotFound = () => {
+    return (
+        <PageNotFoundStyles>
+            <h2>404 | Page not found</h2>
+            <a href="/">Back to home</a>
+        </PageNotFoundStyles>
+    );
+}
+
+export default PageNotFound
