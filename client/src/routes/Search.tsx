4f17d3ca1761222e454604723e79da20a9d316d9
amirfakhrullah <amirfakhrullah96@gmail.com>
2022-09-11 01:56:35 +0800

    search page done

--- client/src/routes/Search.tsx
+++ client/src/routes/Search.tsx
@@ -1,21 +1,54 @@
 import LessonCard from "components/CalendarView/LessonCard";
 import { PageWithSidebar } from "components/Containers/PageWithSidebar";
 import Loader from "components/Loader/Loader";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import useFilterLessonsByTags from "../hooks/useFilterLessonsByTags";
 import styled from "styled-components";
+import { InputFormStyles } from "styles/AuthFormStyles";
+import { useState } from "react";
+
+const FieldContainer = styled.div`
+  width: 60vw;
+  padding-bottom: 20px;
+`;
 
 const Container = styled.div`
+  max-width: 80vw;
   width: 100%;
   display: flex;
   gap: 10px;
   flex-wrap: wrap;
 `;
 
+const PCenter = styled.p`
+  text-align: center;
+  width: 100%;
+`;
+
 function Search() {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const tag = searchParams.get("tag") || undefined;
   const { isLoading, filteredLessonNotes } = useFilterLessonsByTags(tag);
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.currentTarget.value);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (searchValue.length === 0) {
+      return;
+    }
+    if (e.key === "Enter") {
+      navigate({
+        search: `?tag=${searchValue}`,
+      });
+    }
+  };
+
+  console.log(searchValue);
 
   if (isLoading) {
     return (
@@ -28,7 +61,17 @@ function Search() {
   return (
     <PageWithSidebar>
       <p>Search through your past lessons on Notum</p>
-      {/* TODO: search bar */}
+
+      <FieldContainer>
+        <InputFormStyles>
+          <input
+            placeholder="Type-in the tag and click enter..."
+            onChange={handleSearch}
+            onKeyDown={handleKeyPress}
+          />
+        </InputFormStyles>
+      </FieldContainer>
+
       <Container>
         {filteredLessonNotes.length > 0 &&
           filteredLessonNotes.map((lessonNote) => (
@@ -43,6 +86,10 @@ function Search() {
               }}
             />
           ))}
+
+        {filteredLessonNotes.length === 0 && (
+          <PCenter>No Lessons Found</PCenter>
+        )}
       </Container>
     </PageWithSidebar>
   );
